02fd2bedc20fa14cccd6d59c3814f3ad73e188d7
AljazMusic11 <63785933+AljazMusic11@users.noreply.github.com>
2025-04-09 09:57:27 +0200

    Update TweetPoster.py

--- TweetPoster.py
+++ TweetPoster.py
@@ -2,6 +2,9 @@ import tweepy
 import os
 import openai
 import random
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 API_KEY = os.getenv("API_KEY")
